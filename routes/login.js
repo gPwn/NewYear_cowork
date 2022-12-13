4e0b06b6f24a631e24962a662d7fc0d4cc89ad42
soogineer <ysooroh54@gmail.com>
2022-12-13 16:24:01 +0900

    login기능 구현중

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,59 @@
 const express = require("express");
-const router = express.Router();
+const Joi = require('joi');
+const { Users } = require('../models');
+const { Op } = require('sequelize');
+const jwt = require('jsonwebtoken');
+const authLoginUserMiddleware = require('../middlewares/authLoginUserMiddleware');
 
-// router.get('/', (req, res) => {
-//   res.send("Server: login");
-//   });
+const router = express.Router();
+require('dotenv').config();
+
+const loginSchema = Joi.object({
+    nickname: Joi.string().required(),
+    password: Joi.string().required(),
+});
+
+
+router.post('/', authLoginUserMiddleware, async (req, res) => {
+try {
+const { nickname, password } = await loginSchema.validateAsync(req.body);
+const user = await Users.findOne({
+    where: {
+        [Op.and]: [{ nickname }, { password }],
+    },
+});
+
+if (!user) {
+    return res.status(412).send({
+        errorMessage: '닉네임 또는 패스워드를 확인해주세요.',
+    });
+}
+
+if (nickname !== password) {
+    return res.status(412).send({
+        errorMessage: '닉네임 또는 패스워드가 일치하지 않습니다.',
+    });
+}
+
+    const expires = new Date();
+    expires.setMinutes(expires.getMinutes() + 60);
+
+    const token = jwt.sign(
+        { userId: user.userId },
+        process.env.SECRET_KEY,
+        { expiresIn: "60m" }
+      );
+
+      res.cookie(process.env.COOKIE_NAME, `Bearer ${token}`, {
+        expires: expires,
+      });
+      return res.status(200).json({ token });
+    } catch (error) {
+        console.log(`${req.method} ${req.originalUrl} : ${error.message}`);
+        return res.status(400).send({
+          errorMessage: '로그인에 실패하였습니다.',
+        });
+      }
+    });
 
 module.exports = router;
