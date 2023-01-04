4b8c51ad532e31d6cc00dcd6730371c95bb1cd37
gPwn <hyeju3014@gmail.com>
2023-01-04 12:08:43 +0900

    cicdtest

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.use("/login", require("./login.js"));
-router.use("/signup", require("./signup.js"));
-router.use("/lists", require("./lists.js"));
+router.use('/login', require('./login.js'));
+router.use('/signup', require('./signup.js'));
+router.use('/lists', require('./lists.js'));
 // router.use("/lists", require("./workspace/listsPost.js"));
 // router.use("/lists", require("./workspace/listsGet.js"));
 // router.use("/lists", require("./workspace/listsUpdateDelete.js"));
 // const config = require(__dirname + '/../config/config.js')[env];
 
-router.get("/", (req, res) => {
-  res.send("Server: api");
+router.get('/', (req, res) => {
+    res.send('Server: api cicdtest!!');
 });
 
 module.exports = router;
